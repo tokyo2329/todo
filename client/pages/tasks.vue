67a76f5f4dd56e7d7420d1e1ada6406fe9ef16f5
tokyo2329 <mateusz.kociola@icloud.com>
2024-03-09 19:49:05 +0100

    feat(client): add request error handling

--- client/pages/tasks.vue
+++ client/pages/tasks.vue
@@ -2,33 +2,60 @@
 const runtimeConfig = useRuntimeConfig();
 const toast = useToast();
 
+const tasks = ref([]);
 const new_task_modal_open = ref(false);
 const edit_task_modal_open = ref(false);
 const edit_task_reference = ref(null);
 
-const { data: tasks } = await useFetch(runtimeConfig.public.apiUrl + "/tasks", {
-  watch: [new_task_modal_open, edit_task_modal_open],
-});
+const { data, pending, refresh } = await useFetch(
+  runtimeConfig.public.apiUrl + "/tasks",
+  {
+    server: false,
+    watch: [new_task_modal_open, edit_task_modal_open],
+    onRequestError({ error }) {
+      toast.add({
+        title: "Fetching tasks failed",
+        color: "red",
+      });
+    },
+    onResponse({ response }) {
+      console.log("data fetched");
+      tasks.value = response._data;
+    },
+  },
+);
 
 const handle_check_completed = async (task) => {
-  task.is_done = !task.is_done;
-
   await useFetch(runtimeConfig.public.apiUrl + "/tasks/" + task.id, {
     method: "PUT",
-    body: task,
+    body: {
+      id: task.id,
+      name: task.name,
+      is_done: !task.is_done,
+    },
+    onRequestError() {
+      toast.add({ title: "Error while updating task!", color: "red" });
+    },
+    onResponse() {
+      toast.add({ title: "Task has been updated!", color: "blue" });
+    },
   });
 
-  toast.add({ title: "Task has been updated!", color: "blue" });
+  refresh();
 };
 
 const handle_delete = async (task) => {
   await useFetch(runtimeConfig.public.apiUrl + "/tasks/" + task.id, {
     method: "DELETE",
+    onRequestError() {
+      toast.add({ title: "Error while deleting task!", color: "red" });
+    },
+    onResponse() {
+      toast.add({ title: "Task has been deleted!", color: "primary" });
+    },
   });
 
-  tasks.value = tasks.value.filter((t) => t.id != task.id);
-
-  toast.add({ title: "Task has been deleted", color: "red" });
+  refresh();
 };
 
 const columns = [
@@ -73,9 +100,17 @@ const options_dropdown = (task) => [
   <UContainer class="space-y-8">
     <h1 class="text-5xl font-semibold">Task list</h1>
 
-    <UButton label="Add new" @click="new_task_modal_open = true" />
+    <div class="flex gap-x-2 items-center">
+      <UButton label="Add new" @click="new_task_modal_open = true" />
+      <UButton
+        label="Refresh"
+        variant="outline"
+        @click="refresh"
+        :loading="pending"
+      />
+    </div>
 
-    <UTable :columns="columns" :rows="tasks">
+    <UTable :columns="columns" :rows="tasks" :loading="pending">
       <template #is_done-data="{ row }">
         <UCheckbox
           v-model="row.is_done"
